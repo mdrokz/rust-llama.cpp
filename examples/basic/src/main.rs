cb77871e21f82464c1e490612a302868852996ac
Ivan Porto Carrero <ivan@flanders.co.nz>
2024-02-12 19:49:58 -0800

    undo changes to example

--- examples/basic/src/main.rs
+++ examples/basic/src/main.rs
@@ -2,34 +2,33 @@ use llama_cpp_rs::{
     options::{ModelOptions, PredictOptions},
     LLama,
 };
-use std::io::Write;
 
 fn main() {
     let model_options = ModelOptions::default();
 
     let llama = LLama::new(
-        "../../models/tinyllama-chat/tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf".into(),
-        // "/home/ivan/github/ggerganov/llama/models/7B/ggml-model-q4_0.gguf".into(),
+        "./<your model>.bin".into(),
         &model_options,
     )
     .unwrap();
 
     let predict_options = PredictOptions {
-        tokens: 400,
-        threads: 8,
+        tokens: 0,
+        threads: 14,
         top_k: 90,
         top_p: 0.86,
         token_callback: Some(Box::new(|token| {
-            print!("{}", token);
+            println!("token1: {}", token);
 
-            std::io::stdout().flush().unwrap();
             true
         })),
         ..Default::default()
     };
 
-    let prompt = "Building a website can be done in 10 simple steps:\nStep 1:";
-    println!();
-    println!("{prompt}");
-    llama.predict(prompt.into(), predict_options).unwrap();
+    llama
+        .predict(
+            "what are the national animals of india".into(),
+             predict_options,
+        )
+        .unwrap();
 }
